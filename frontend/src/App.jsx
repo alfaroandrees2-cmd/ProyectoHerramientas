c31d412f4468983ac14ac9906c43f7680ef57b98
TRABAJO <Lab73.peru@gmail.com>
2026-05-19 19:36:57 -0500

    mis avances jajaja

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import Login from './componentes/Login'
 import Registro from './componentes/Registro'
 import Dashboard from './componentes/Dashboard'
 import './App.css'
